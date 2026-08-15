5877bd9909eb74a11ce61487f3cc68fd7e31592a
rainelysbatista0515-droid <rainelysbatista0515@gmail.com>
2026-08-14 22:11:04 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "discord.gg/lure", // required by Discord API, not shown in the client
+        name: "Custom Status", // required by Discord API, not shown in the client
         state: "we love rain",     // this is what people actually see
         type: 4,               // Custom
       },

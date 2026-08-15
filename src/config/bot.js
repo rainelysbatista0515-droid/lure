70e72258f07ec07d546d33d203df6751b74790a3
rainelysbatista0515-droid <rainelysbatista0515@gmail.com>
2026-08-14 22:10:40 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "discord.gg/lure", // required by Discord API, not shown in the client
         state: "we love rain",     // this is what people actually see
-        type: 3,               // Custom
+        type: 4,               // Custom
       },
     ],
   },
